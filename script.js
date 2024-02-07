cbcb985fa8ef5b5f627440f2f7cfbd0157c85e0e
LMGP2002 <luiguerrero@uniboyaca.edu.co>
2024-02-07 14:18:20 -0500

    solución borrar solo un carácter

--- script.js
+++ script.js
@@ -11,4 +11,11 @@ function agregarALaPantalla(value) {
   function limpiarPantalla() {
     document.getElementById('pantalla').value = '';
   }
+
+
+  function deshacer() {
+    const valorPantalla = document.getElementById('pantalla').value;
+    document.getElementById('pantalla').value = valorPantalla.slice(0,-1);
+  }
+  
   
